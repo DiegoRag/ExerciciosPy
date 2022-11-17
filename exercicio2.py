e473c2b79d69922c0fe462ec09a99fbcd53087b6
DiegoRag <ragozinidiego@gmail.com>
2022-11-17 14:43:40 -0300

    Exercicios python

---
+++ exercicio2.py
@@ -0,0 +1,3 @@
+class exercicio_2:
+   nome = input('diga seu nome:')
+   print(f'Tudo certo {nome}?')
---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+class exercicio_3:
+   num1 = int(input("Escolha o primeiro numero: "))
+
+   num2 = int(input('Escolha o segundo numero: '))
+
+
+   resultado = print(f'A soma do numero {num1} e {num2} vai ser igual a {num1 + num2}')
